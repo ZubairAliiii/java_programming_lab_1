b6fe9a4879dae370d178a011dd0927ac9b6c7c92
Zubair Alii <115203752+ZubairAliiii@users.noreply.github.com>
2022-10-09 09:21:19 -0700

    Update QOne.java

--- QOne.java
+++ QOne.java
@@ -11,15 +11,18 @@ public static void main(String[] org)
 {
 int x=10;
 int y=20;
+ System.out.println("These are integers variables:");
 System.out.println(x);
 System.out.println(y);
-
+  
+System.out.println("These are float variables");
 double p=30.8;
 double q=70.6;
 
 System.out.println(p);
 System.out.println(q);
 
+System.out.println("These are characters variables:");
 char a='a';
 char b='b';
 System.out.println(a);
